25a4c59e273a0cda426252f535375a7d32a3befd
Oleksandr <olexander11441144@gmail.com>
2023-10-24 15:58:24 +0300

    fixed problem with body field in messages collection

--- components/messages/ShortcutBodyView.jsx
+++ components/messages/ShortcutBodyView.jsx
@@ -1,9 +1,11 @@
 export default function BlockAttachmentsShow({ record, resource }) {
-  const currentWebEndpoint = record.params.body;
+  const currentBody = record.params.body;
 
   return `${
-    currentWebEndpoint.length < 11
-      ? currentWebEndpoint
-      : currentWebEndpoint.slice(0, 10) + "..."
+    currentBody
+      ? currentBody.length < 11
+        ? currentBody
+        : currentBody.slice(0, 10) + "..."
+      : ""
   }`;
 }
